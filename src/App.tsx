50ed280b0db2261f6bdc44c096cb489222e8882f
HugoBDev <151855094+HugoBDev@users.noreply.github.com>
2024-03-11 16:14:21 +0100

    init welcome page

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+
+
+
+function App() {
+ 
+  return (
+    <>
+      <div className="container">
+        <img src="" alt="logo" />
+        <p className="title">Bientôt en ligne</p>
+        <p>Vous pourrez retrouver ici les créations de l’amienoise qui dessine, sur une carte d’Amiens</p>
+      </div>
+      <div className="social-medias">
+        <p>pour patientez, retrouvez mes creations sur les réseaux</p>
+        <div className="social-bubble-container">
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
