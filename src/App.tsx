e4e21c1ea79d098f6dd1633231748e147c25bb26
HugoBDev <151855094+HugoBDev@users.noreply.github.com>
2024-03-11 18:05:21 +0100

    adding a way better background video

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <div className="background-video">
         <video autoPlay muted controls={false}>
           <source
-            src="https://dtn44hfc1xtr4.cloudfront.net/x/f/xfc9z5k.mp4?Expires=1710257844&Signature=ONqFluOQ1S6V~urxZ9RWLejzMEFlbtNcOU5-cw5O300gFRg7EKtUtX~R-U4YspIwvskFFD8wNCFR~wbT5HEh1hw562y4fpwFk-OcoAIf2JG7Nu2wHOOoBdVobnNaZydg7rjC7pGVWoEn1SXkrS2jD56nEqraxRH2bQr6gPsrrMeC-3BU4KY4bpoTYy-1pCp1MVxZzdPs68JjeeTHJQ10j9r6z0wsUoxOAj3apdT5ytqqOQIHLoZB76Lu1pUv7WpHkmWV0vwWs9zSHEitU577cqch0OP1vdznJ2lIKcFKTM8GXt0q0CIM7Ql7qk81SgTC9aHfNFjcBwmN8eNdAKzHsQ__&Key-Pair-Id=APKAIAOH4YUSYMI7SPBA"
+            src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
             type="video/mp4"
           />
           Video not supported.
