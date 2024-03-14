f657d33238ffa154b2b410e738777f5558170112
HugoBDev <151855094+HugoBDev@users.noreply.github.com>
2024-03-14 17:10:30 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,6 @@ function App() {
         <h3>Bientôt en ligne</h3>
         <p>
           Ici, vous pourrez retrouver les dessins et aquarelles de l’amiénoise qui dessine...
-          
         </p>
 
       </div>
